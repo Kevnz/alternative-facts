25a5e7c24c19b704801e268d0f99f9ad57b2b27c
Kevin Isom <kevin.isom@gmail.com>
2019-02-24 22:47:55 +1300

    remove it

--- tools/array.js
+++ tools/array.js
@@ -1,35 +1,5 @@
 "use strict";
 
-if (!Array.prototype.includes) {
-  Object.defineProperty(Array.prototype, 'includes', {
-    value: function value(searchElement, fromIndex) {
-      if (this == null) {
-        throw new TypeError('"this" is null or not defined');
-      }
-
-      var o = Object(this);
-      var len = o.length >>> 0;
-
-      if (len === 0) {
-        return false;
-      }
-
-      var n = fromIndex | 0;
-      var k = Math.max(n >= 0 ? n : len - Math.abs(n), 0);
-
-      while (k < len) {
-        if (o[k] === searchElement) {
-          return true;
-        }
-
-        k++;
-      }
-
-      return false;
-    }
-  });
-}
-
 if (!Array.prototype.empty) {
   Object.defineProperty(Array.prototype, 'empty', {
     value: function value(searchElement, fromIndex) {
