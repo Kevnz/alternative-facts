2b065dcd2e8c8c7702ef934127534488c7732af5
Kevin Isom <kevin.isom@gmail.com>
2019-02-25 07:04:31 +1300

    Remove from helpers from src and base

--- tools/array.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-if (!Array.prototype.empty) {
-  Object.defineProperty(Array.prototype, 'empty', {
-    value: function value(searchElement, fromIndex) {
-      this.splice(0, this.length);
-    }
-  });
-}
